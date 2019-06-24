4295ab3bc4f031b41473db493484365ba7bf71ea
praveen70 <praveen.mr288@gmail.com>
2019-06-24 23:18:22 +0530

    ui

--- client/src/components/Imageslider/index.js
+++ client/src/components/Imageslider/index.js
@@ -5,8 +5,9 @@ import { Carousel } from 'antd';
 
 const fadeImages = [
   'https://rukminim1.flixcart.com/flap/3376/560/image/676ca540c0fe7dd6.jpg?q=50',
-    'https://rukminim1.flixcart.com/flap/3376/560/image/676ca540c0fe7dd6.jpg?q=50',
-  'https://rukminim1.flixcart.com/flap/3376/560/image/676ca540c0fe7dd6.jpg?q=50'
+    'https://rukminim1.flixcart.com/flap/3376/560/image/b449b8dca0f0b945.jpg?q=50',
+  'https://rukminim1.flixcart.com/flap/3376/560/image/9d1050658f91ceaf.jpg?q=50',
+  "https://rukminim1.flixcart.com/flap/3376/560/image/98d0c64d37a5e4cc.jpg?q=50",
 ];
  
 const fadeProperties = {
@@ -52,6 +53,11 @@ const Slider = () => {
       <div className="each-fade" style={{marginTop: 20}}>
         <div className="image-container">
           <img src={fadeImages[2]} style={{ width: '100%'}} />
+        </div>
+      </div>
+      <div className="each-fade" style={{marginTop: 20}}>
+        <div className="image-container">
+          <img src={fadeImages[3]} style={{ width: '100%'}} />
         </div>
       </div>
     </Fade>
